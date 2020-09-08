8280baf1d8b01ec7753d1bf7f2f839cd34f3e096
王啸然 <56494457+bistuwangxiaoran@users.noreply.github.com>
2020-09-08 16:06:05 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.Helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String str="ds";
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
